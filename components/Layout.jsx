e52114af5848a40134593c7518a8cb3a8be59a85
Khalfan1998 <khalfan.ikns@gmail.com>
2022-01-17 19:40:49 +0300

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,7 @@ import Footer from "./Footer";
 const Layout = ({children}) => (
     <>
     <Head>
-        <title>Real Estate</title>
+        <title>7amood Estate</title>
     </Head>
     <Box maxWidth="1280px" m="auto">
         <header>
